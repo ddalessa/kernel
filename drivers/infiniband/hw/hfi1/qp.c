f61009a3f1dbfc35ef5b99e65a0e36e97e66a3ce
Kaike Wan <kaike.wan@intel.com>
2018-06-18 08:57:34 -0400

    IB/hfi1: Enable TID RDMA protocol
    This patch enables TID RDMA protocol by converting a qualified RDMA
    READ/WRITE request into a TID RDMA READ/WRITE request internally:
    (1) The TID RDMA cability must be enabled;
    (2) The request must start on a 4K page boundary; For read, all receiving
    buffers must start on 4K page boundaries;
    (3) The request length must be a multiple of 4K and must be larger or
    equal to 256K. For read, each receiving buffer length must be a multiple
    of 4K.
    
    Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>

--- drivers/infiniband/hw/hfi1/qp.c
+++ drivers/infiniband/hw/hfi1/qp.c
@@ -304,26 +304,28 @@ void hfi1_modify_qp(struct rvt_qp *qp, struct ib_qp_attr *attr,
 }
 
 /**
- * hfi1_check_send_wqe - validate wqe
+ * hfi1_setup_wqe - setup the wqe
  * @qp - The qp
  * @wqe - The built wqe
  *
- * validate wqe.  This is called
- * prior to inserting the wqe into
- * the ring but after the wqe has been
- * setup.
+ * Perform setup of the wqe.  This is called
+ * prior to inserting the wqe into the ring but after
+ * the wqe has been setup by RDMAVT. This function
+ * allow the driver the opportunity to perform
+ * validation and additional setup of the wqe.
  *
  * Returns 0 on success, -EINVAL on failure
  *
  */
-int hfi1_check_send_wqe(struct rvt_qp *qp,
-			struct rvt_swqe *wqe)
+int hfi1_setup_wqe(struct rvt_qp *qp,
+		   struct rvt_swqe *wqe)
 {
 	struct hfi1_ibport *ibp = to_iport(qp->ibqp.device, qp->port_num);
 	struct rvt_ah *ah;
 
 	switch (qp->ibqp.qp_type) {
 	case IB_QPT_RC:
+		hfi1_setup_tid_rdma_wqe(qp, wqe);
 	case IB_QPT_UC:
 		if (wqe->length > 0x80000000U)
 			return -EINVAL;
