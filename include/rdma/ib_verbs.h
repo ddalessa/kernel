bc1f86d4831997d86d0af5c88f27c6933ddc4db0
Gary Leshner <Gary.S.Leshner@intel.com>
2019-12-11 05:50:10 -0500

    IB/hfi1: Add functions to transmit datagram ipoib packets
    This patch implements the mechanism to accelerate the transmit side of
    a multiple transmit queue RDMA netdev by submitting the packets to
    the SDMA engine directly instead of sending through the verbs layer.
    
    This patch also changes the UD/SEND_ONLY op to output the entropy value
    in byte 0 of deth[1]. UD/SEND_ONLY_WITH_IMMEDIATE uses the previous
    behavior with no entropy value being output.
    
    The code in the ipoib rdma netdev which submits tx requests upon
    successful submission will call trace_sdma_output_ibhdr to output
    the ibhdr to the trace buffer.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Gary Leshner <Gary.S.Leshner@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -2198,6 +2198,7 @@ struct rdma_netdev {
 	void              *clnt_priv;
 	struct ib_device  *hca;
 	u8                 port_num;
+	int                mtu;
 
 	/*
 	 * cleanup function must be specified.
